c29662d3f169469872c48ca07f08d43fe7b96378
JillMOry <oryjill@gmail.com>
2020-07-12 19:16:01 -0500

    with map

--- src/redux/state.js
+++ src/redux/state.js
@@ -13,7 +13,9 @@ export default {
 				"Artisan stone baked pizzas, and wings. We are the Round Rock location of Georgetown pizza favorite 600 Degrees Pizzeria and Drafthouse",
 			address: "201 East Main St., Suite 102 Round Rock, Texas 78664",
 			phone: "(512) 580-0941",
-			hours: "11:00 AM - 8:30 PM"
+			hours: "11:00 AM - 8:30 PM",
+			latitude: "30.509640",
+			longitude: "-97.677395"
 		},
 		{
 			id: 2,
@@ -21,7 +23,9 @@ export default {
 			description: "American (Traditional), Breakfast & Brunch, Coffee & Tea ",
 			address: "2120 N Mays St., Round Rock, TX 78664",
 			phone: "(512) 879-2827",
-			hours: "24 hours!"
+			hours: "24 hours!",
+			latitude: "30.539508",
+			longitude: "-97.688561"
 		},
 		{
 			id: 3,
@@ -29,7 +33,9 @@ export default {
 			description: "Sandwiches, Cheesesteaks, Pizza ",
 			address: "3200 Greenlawn Blvd Ste 120 Round Rock, TX 78664",
 			phone: "(512) 614-4027",
-			hours: "11:00 AM - 9:00 PM"
+			hours: "11:00 AM - 9:00 PM",
+			latitude: "30.480662",
+			longitude: "-97.661490"
 		}
 	]
 };
